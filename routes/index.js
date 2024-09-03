5e88058f56ddac247c0573cfa15339e7fc6a3df8
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-09-03 23:18:51 +0100

    First API

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import { Express, Next } from 'express';
+import AppController from '../controllers/AppController';
+
+/**
+ * Initialize the routes of the api.
+ * @param {Express} api - The express apilication object.
+ * @returns {void} Void
+ */
+const initializeRoutes = (api) => {
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
+};
+
+export default initializeRoutes;
